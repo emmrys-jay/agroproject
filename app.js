4be2d716090ef9d10337ccaba2bbd4ef84139cb6
Emmrys-Jay <jonathanemma121@gmail.com>
2023-06-11 14:28:57 +0100

    fix: fix cors error

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const errorHandler = require("./middlewares/errorHandler");
 const connectDb = require("./config/dbConnection");
 const dotenv = require("dotenv").config();
+const cors = require("cors")
 
 app = express();
 connectDb();
@@ -9,6 +10,7 @@ connectDb();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors());
 app.use("/api/products", require("./routes/productRoutes"));
 app.use(errorHandler);
 
